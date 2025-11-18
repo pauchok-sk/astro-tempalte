df43cdd7a99a2bbd8a123fe291161ea2e30ec3c9
pauchok-sk <pauchoooooook@gmail.com>
2025-11-18 10:08:30 +0300

    scripts

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: false,
+
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "src/scripts/*",
+            dest: "scripts/",
+          },
+        ],
+      }),
+    ],
+    build: {
+      minify: false,
+    },
+  },
+});
