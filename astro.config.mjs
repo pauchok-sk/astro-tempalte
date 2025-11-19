c611989f506967a195fe4ad83b6a44d0ea3d7beb
pauchok-sk <pauchoooooook@gmail.com>
2025-11-20 00:03:04 +0300

    p[oqld

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 export default defineConfig({
   site: 'https://pauchok-sk.github.io',
   base: "/dist",
+  
   compressHTML: false,
   vite: {
     plugins: [
